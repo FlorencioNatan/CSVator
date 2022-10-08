e20172b73ae0d4abc55c23fbee274121c9a27633
Florencio Natan <florencionatan@gmail.com>
2022-10-08 16:19:33 -0300

    Add Double type equality and difference comparsion

--- src/org/csvator/interpreter/environment/operators/comparsion/Different.java
+++ src/org/csvator/interpreter/environment/operators/comparsion/Different.java
@@ -2,6 +2,7 @@ package org.csvator.interpreter.environment.operators.comparsion;
 
 import org.csvator.interpreter.environment.Environment;
 import org.csvator.interpreter.parsingTable.BooleanValue;
+import org.csvator.interpreter.parsingTable.DoubleValue;
 import org.csvator.interpreter.parsingTable.IntegerValue;
 import org.csvator.interpreter.parsingTable.ValueInterface;
 
@@ -10,12 +11,25 @@ public class Different extends EqualityOperator {
 	@Override
 	public ValueInterface apply(ValueInterface lho, ValueInterface rho, Environment env) {
 		boolean result = false;
+
+		if (lho instanceof IntegerValue && rho instanceof DoubleValue) {
+			result = ((IntegerValue) lho).getIntValue(env) != ((DoubleValue) rho).getDoubleValue(env);
+		}
+
+		if (lho instanceof DoubleValue && rho instanceof IntegerValue) {
+			result = ((DoubleValue) lho).getDoubleValue(env) != ((IntegerValue) rho).getIntValue(env);
+		}
+
 		if (lho.getClass() != rho.getClass()) {
 			return this.createResult(lho, rho, result, env);
 		}
 
 		if (lho instanceof IntegerValue) {
 			result = ((IntegerValue) lho).getIntValue(env) != ((IntegerValue) rho).getIntValue(env);
+		}
+
+		if (lho instanceof DoubleValue) {
+			result = ((DoubleValue) lho).getDoubleValue(env) != ((DoubleValue) rho).getDoubleValue(env);
 		}
 
 		if (lho instanceof BooleanValue) {
