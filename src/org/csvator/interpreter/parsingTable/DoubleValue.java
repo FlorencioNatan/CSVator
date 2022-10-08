ea358d20904c887aaff7c998c528c7c8685d9192
Florencio Natan <florencionatan@gmail.com>
2022-10-08 15:19:09 -0300

    Implement the DoubleValue class

---
+++ src/org/csvator/interpreter/parsingTable/DoubleValue.java
@@ -0,0 +1,45 @@
+package org.csvator.interpreter.parsingTable;
+
+import org.csvator.interpreter.environment.Environment;
+
+public class DoubleValue implements ValueInterface {
+
+	String id;
+	double value;
+
+	public DoubleValue(String id, String strValue) {
+		this.id = id;
+		this.value = Double.valueOf(strValue);
+	}
+
+	public DoubleValue(String id, double value) {
+		this.id = id;
+		this.value = value;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public ValueInterface evaluate(Environment env) {
+		return this;
+	}
+
+	public double getDoubleValue(Environment env) {
+		return this.value;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return Double.toString(value);
+	}
+
+	@Override
+	public Class getTypeClass() {
+		return this.getClass();
+	}
+
+}
