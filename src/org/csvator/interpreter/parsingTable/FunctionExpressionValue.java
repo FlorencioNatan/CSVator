7a4e7f000bea4906b274e953a13c36b4c2987ea8
Florencio Natan <florencionatan@gmail.com>
2022-10-16 16:17:41 -0300

    Adjust the FunctionExpressionValue to use FunctionCall and FunctionTypeValue classes

--- src/org/csvator/interpreter/parsingTable/FunctionExpressionValue.java
+++ src/org/csvator/interpreter/parsingTable/FunctionExpressionValue.java
@@ -3,18 +3,20 @@ package org.csvator.interpreter.parsingTable;
 import java.util.LinkedList;
 
 import org.csvator.interpreter.environment.Environment;
-import org.csvator.interpreter.parsingTable.function.FunctionValue;
+import org.csvator.interpreter.parsingTable.function.FunctionCall;
 import org.csvator.interpreter.parsingTable.function.TypeMismatchException;
+import org.csvator.interpreter.parsingTable.typeValues.FunctionTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.TypeValueInterface;
 
 public class FunctionExpressionValue implements ExpressionValueInterface {
 
 	String id;
-	FunctionValue function;
+	FunctionCall call;
 	LinkedList<ValueInterface> expressions;
 
-	public FunctionExpressionValue(String id, FunctionValue function, LinkedList<ValueInterface> expressions) {
+	public FunctionExpressionValue(String id, FunctionCall call, LinkedList<ValueInterface> expressions) {
 		this.id = id;
-		this.function = function;
+		this.call = call;
 		this.expressions = expressions;
 	}
 
@@ -25,18 +27,17 @@ public class FunctionExpressionValue implements ExpressionValueInterface {
 
 	@Override
 	public ValueInterface evaluate(Environment env) {
-		Environment local = function.createLocalEnvironment(expressions, env);
-		ValueInterface result = function.evaluate(local);
-		if (result.getTypeClass() != function.getReturnType()) {
-			throw new TypeMismatchException("Type mismatch on function " + this.id.trim() + " return. Expected " + function.getReturnType() + " found " + result.getTypeClass());
+		Environment local = call.createLocalEnvironment(expressions, env);
+		ValueInterface result = call.evaluate(local);
+		if (result.getType() != call.getReturnType(env)) {
+			throw new TypeMismatchException("Type mismatch on function " + this.id.trim() + " return. Expected " + call.getReturnType(env) + " found " + result.getType().getId().getClass());
 		}
-		return function.evaluate(local);
+		return call.evaluate(local);
 	}
 
-	@SuppressWarnings("rawtypes")
 	@Override
-	public Class getTypeClass() {
-		return FunctionValue.class;
+	public TypeValueInterface getType() {
+		return new FunctionTypeValue(null);
 	}
 
 }
