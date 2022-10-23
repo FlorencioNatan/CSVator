3f1586c8baf30cedf6be8516ae66220c67c4354f
Florencio Natan <florencionatan@gmail.com>
2022-10-23 12:01:06 -0300

    Processes the function call of a varible with a value of a function

--- src/org/csvator/interpreter/parsingTable/function/FunctionCall.java
+++ src/org/csvator/interpreter/parsingTable/function/FunctionCall.java
@@ -3,6 +3,7 @@ package org.csvator.interpreter.parsingTable.function;
 import java.util.LinkedList;
 
 import org.csvator.interpreter.environment.Environment;
+import org.csvator.interpreter.parsingTable.FunctionExpressionValue;
 import org.csvator.interpreter.parsingTable.ValueInterface;
 import org.csvator.interpreter.parsingTable.typeValues.FunctionTypeValue;
 import org.csvator.interpreter.parsingTable.typeValues.TypeValueInterface;
@@ -16,8 +17,14 @@ public class FunctionCall implements ValueInterface {
 	}
 
 	public Environment createLocalEnvironment(LinkedList<ValueInterface> values, Environment father) throws TypeMismatchException {
-		FunctionValue value = (FunctionValue) father.getValueOf(id);
-		return value.createLocalEnvironment(values, father);
+		ValueInterface value = father.getValueOf(id);
+		FunctionValue functionValue;
+		if (value instanceof FunctionExpressionValue) {
+			functionValue = (FunctionValue) value.evaluate(father);
+		} else {
+			functionValue = (FunctionValue) value;
+		}
+		return functionValue.createLocalEnvironment(values, father);
 	}
 
 	@Override
@@ -27,13 +34,25 @@ public class FunctionCall implements ValueInterface {
 
 	@Override
 	public ValueInterface evaluate(Environment env) {
-		FunctionValue value = (FunctionValue) env.getValueOf(id);
-		return value.evaluate(env);
+		ValueInterface value = env.getValueOf(id);
+		FunctionValue functionValue;
+		if (value instanceof FunctionExpressionValue) {
+			functionValue = (FunctionValue) value.evaluate(env);
+		} else {
+			functionValue = (FunctionValue) value;
+		}
+		return functionValue.apply(env);
 	}
 
 	public TypeValueInterface getReturnType(Environment env) {
-		FunctionValue value = (FunctionValue) env.getValueOf(id);
-		return value.getReturnType();
+		ValueInterface value = env.getValueOf(id);
+		FunctionValue functionValue;
+		if (value instanceof FunctionExpressionValue) {
+			functionValue = (FunctionValue) value.evaluate(env);
+		} else {
+			functionValue = (FunctionValue) value;
+		}
+		return functionValue.getReturnType();
 	}
 
 	@Override
