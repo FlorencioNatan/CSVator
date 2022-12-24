ec2d4b67a02141b0bea7a732ed262b23bdc2e155
Florencio Natan <florencionatan@gmail.com>
2022-12-24 12:14:32 -0300

    Implements some basic features of the IDE

--- src/org/csvator/main/gui/CSVatorIDE.java
+++ src/org/csvator/main/gui/CSVatorIDE.java
@@ -4,17 +4,21 @@ import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -30,6 +34,7 @@ import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.SwingConstants;
 
 public class CSVatorIDE extends JFrame {
 
@@ -52,9 +57,10 @@ public class CSVatorIDE extends JFrame {
 		setJMenuBar(menuBar);
 		
 				JMenu mnFile = new JMenu("File");
+				mnFile.setMnemonic(KeyEvent.VK_F);
 				menuBar.add(mnFile);
 
-						JMenuItem mntmOpen = new JMenuItem("Open");
+						JMenuItem mntmOpen = new JMenuItem("Open", KeyEvent.VK_O);
 						mntmOpen.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent e) {
 								JFileChooser fileChooser = new JFileChooser();
@@ -82,31 +88,66 @@ public class CSVatorIDE extends JFrame {
 						});
 						mnFile.add(mntmOpen);
 						
-						JMenuItem mntmSave = new JMenuItem("Save");
+						JMenuItem mntmSave = new JMenuItem("Save", KeyEvent.VK_S);
+						mntmSave.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								if (filePath == null || filePath.equals("")) {
+									saveAsTextFile(mntmSave);
+								} else {
+									saveTextFile();
+								}
+							}
+						});
 						mnFile.add(mntmSave);
 						
-						JMenuItem mntmSaveAs = new JMenuItem("Save As ...");
+						JMenuItem mntmSaveAs = new JMenuItem("Save As ...", KeyEvent.VK_A);
+						mntmSaveAs.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								saveAsTextFile(mntmSaveAs);
+							}
+						});
 						mnFile.add(mntmSaveAs);
 						
-						JMenuItem mntmExit = new JMenuItem("Exit");
+						JMenuItem mntmExit = new JMenuItem("Exit", KeyEvent.VK_X);
+						mntmExit.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								CSVatorIDE.this.setDefaultCloseOperation(CSVatorIDE.EXIT_ON_CLOSE);
+								CSVatorIDE.this.dispose();
+							}
+						});
 						mnFile.add(mntmExit);
 						
 						JMenu mnRun = new JMenu("Run");
+						mnRun.setMnemonic(KeyEvent.VK_R);
 						menuBar.add(mnRun);
 						
-						JMenuItem mntmRunFile = new JMenuItem("Run file");
+						JMenuItem mntmRunFile = new JMenuItem("Run file", KeyEvent.VK_F);
+						mntmRunFile.setHorizontalAlignment(SwingConstants.LEFT);
+						mntmRunFile.addActionListener(mnRunFile());
 						mnRun.add(mntmRunFile);
 						
-						JMenuItem mntmRunSelection = new JMenuItem("Run selection");
+						JMenuItem mntmRunSelection = new JMenuItem("Run selection", KeyEvent.VK_S);
+						mntmRunSelection.addActionListener(mnRunSelection());
 						mnRun.add(mntmRunSelection);
 						
-						JMenuItem mntmResetEnvironment = new JMenuItem("Reset Environment");
+						JMenuItem mntmResetEnvironment = new JMenuItem("Reset Environment", KeyEvent.VK_E);
+						mntmResetEnvironment.addActionListener(mnResetEnvironment());
 						mnRun.add(mntmResetEnvironment);
 						
 						JMenu mnHelp = new JMenu("Help");
+						mnHelp.setMnemonic(KeyEvent.VK_H);
 						menuBar.add(mnHelp);
 						
-						JMenuItem mntmAbout = new JMenuItem("About");
+						JMenuItem mntmAbout = new JMenuItem("About", KeyEvent.VK_A);
+						mntmAbout.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								About dialog = new About();
+								dialog.setModal(true);
+								dialog.setLocationRelativeTo(CSVatorIDE.this);
+								dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+								dialog.setVisible(true);
+							}
+						});
 						mnHelp.add(mntmAbout);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -119,24 +160,15 @@ public class CSVatorIDE extends JFrame {
 		contentPane.add(toolBar);
 
 		JButton btnRunFile = new JButton("Run file");
-		btnRunFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String[][] mockData = {
-						{"Line 1, col 1", "Line 1, col 2", "Line 1, col 3"},
-						{"Line 2, col 1", "Line 2, col 2", "Line 2, col 3"},
-						{"Line 3, col 1", "Line 3, col 2", "Line 3, col 3"},
-				};
-				String[] columns = {"Col 1", "Col 2", "Col 3"};
-				TableModel model = new DefaultTableModel(mockData, columns);
-				table.setModel(model);
-			}
-		});
+		btnRunFile.addActionListener(mnRunFile());
 		toolBar.add(btnRunFile);
 
 		JButton btnRunSelection = new JButton("Run selection");
+		btnRunSelection.addActionListener(mnRunSelection());
 		toolBar.add(btnRunSelection);
 		
 		JButton btnResetEnvironment = new JButton("Reset Environment");
+		btnResetEnvironment.addActionListener(mnResetEnvironment());
 		toolBar.add(btnResetEnvironment);
 
 		Component horizontalGlue = Box.createHorizontalGlue();
@@ -162,6 +194,77 @@ public class CSVatorIDE extends JFrame {
 		textPane.setFont(new Font("Fira Code Retina", Font.PLAIN, 14));
 		JScrollPane scrollPaneTextPane = new JScrollPane(textPane);
 		splitPane.setLeftComponent(scrollPaneTextPane);
+	}
+
+	private ActionListener mnRunFile() {
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[][] mockData = {
+						{"Line 1, col 1", "Line 1, col 2", "Line 1, col 3"},
+						{"Line 2, col 1", "Line 2, col 2", "Line 2, col 3"},
+						{"Line 3, col 1", "Line 3, col 2", "Line 3, col 3"},
+				};
+				String[] columns = {"Col 1", "Col 2", "Col 3"};
+				TableModel model = new DefaultTableModel(mockData, columns);
+				table.setModel(model);
+			}
+		};
+	}
+
+	private ActionListener mnRunSelection() {
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[][] mockData = {
+						{"Col 1", "Col 2", "Col 3"},
+				};
+				String[] columns = {"Col 1", "Col 2", "Col 3"};
+				TableModel model = new DefaultTableModel(mockData, columns);
+				table.setModel(model);
+			}
+		};
+	}
+
+	private ActionListener mnResetEnvironment() {
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[][] mockData = {
+						{"Data"},
+				};
+				String[] columns = {"Result"};
+				TableModel model = new DefaultTableModel(mockData, columns);
+				table.setModel(model);
+			}
+		};
+	}
+
+	private void saveAsTextFile(Component parent) {
+		JFileChooser fileChooser = new JFileChooser();
+		int option = fileChooser.showSaveDialog(parent);
+		if (option == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			filePath = file.getAbsolutePath();
+			Path path = Paths.get(filePath);
+			try {
+				BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"), StandardOpenOption.CREATE);
+
+				writer.write(textPane.getText());
+				writer.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	private void saveTextFile() {
+		Path path = Paths.get(filePath);
+		try {
+			BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"), StandardOpenOption.CREATE);
+
+			writer.write(textPane.getText());
+			writer.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 }
