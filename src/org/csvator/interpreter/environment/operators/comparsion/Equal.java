325e235e55d0edc7c5c5e25e5b42bf3c1dc6cf7e
Florencio Natan <florencionatan@gmail.com>
2023-02-15 20:50:10 -0300

    Fix a bug on equal and different comparators

--- src/org/csvator/interpreter/environment/operators/comparsion/Equal.java
+++ src/org/csvator/interpreter/environment/operators/comparsion/Equal.java
@@ -1,6 +1,8 @@
 package org.csvator.interpreter.environment.operators.comparsion;
 
-public class Equal extends ComparsionOperator {
+import org.csvator.interpreter.parsingTable.ValueInterface;
+
+public class Equal extends EqualityOperator {
 
 	@Override
 	protected boolean operationOnInt(int lho, int rho) {
@@ -15,6 +17,21 @@ public class Equal extends ComparsionOperator {
 	@Override
 	protected boolean operationOnString(String lho, String rho) {
 		return lho.equals(rho);
+	}
+
+	@Override
+	protected boolean operationOnBoolean(boolean lho, boolean rho) {
+		return lho == rho;
+	}
+
+	@Override
+	protected boolean operationOnNull(ValueInterface lho, ValueInterface rho) {
+		return lho == rho;
+	}
+
+	@Override
+	protected boolean operationOnDifferentClasses(ValueInterface lho, ValueInterface rho) {
+		return false;
 	}
 
 }
---
+++ src/org/csvator/interpreter/environment/operators/comparsion/EqualityOperator.java
@@ -0,0 +1,179 @@
+package org.csvator.interpreter.environment.operators.comparsion;
+
+import java.time.format.DateTimeFormatter;
+
+import org.csvator.interpreter.environment.Environment;
+import org.csvator.interpreter.environment.operators.OperatorInterface;
+import org.csvator.interpreter.parsingTable.BooleanValue;
+import org.csvator.interpreter.parsingTable.DateTimeValue;
+import org.csvator.interpreter.parsingTable.DateValue;
+import org.csvator.interpreter.parsingTable.DoubleValue;
+import org.csvator.interpreter.parsingTable.IntegerValue;
+import org.csvator.interpreter.parsingTable.StringValue;
+import org.csvator.interpreter.parsingTable.ValueInterface;
+import org.csvator.interpreter.parsingTable.typeValues.BoolTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.DateTimeTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.DateTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.DoubleTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.IntTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.StringTypeValue;
+import org.csvator.interpreter.parsingTable.typeValues.VoidTypeValue;
+
+public abstract class EqualityOperator implements OperatorInterface {
+
+	private BooleanValue comparsionResult;
+
+	public ValueInterface apply(ValueInterface lho, ValueInterface rho, Environment env) {
+		defaultNumericComparsion(lho, rho, env);
+		defaultDateComparsion(lho, rho, env);
+		differentClassComparsion(lho, rho, env);
+		intComparsion(lho, rho, env);
+		stringComparsion(lho, rho, env);
+		datetimeComparsion(lho, rho, env);
+		booleanComparsion(lho, rho, env);
+		nullComparsion(lho, rho, env);
+
+		return comparsionResult;
+	}
+
+	private void booleanComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		if (lho.getType() == BoolTypeValue.getInstance()) {
+			BooleanValue booleanLho = this.castToBooleanValue(lho);
+			BooleanValue booleanRho = this.castToBooleanValue(rho);
+			boolean result = this.operationOnBoolean(booleanLho.getBooleanValue(), booleanRho.getBooleanValue());
+			comparsionResult = this.createResult(lho, rho, result, env);
+		}
+	}
+
+	private void datetimeComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		if (lho.getType() == DateTimeTypeValue.getInstance()) {
+			DateTimeValue datetimeLho = this.castToDateTimeValue(lho);
+			DateTimeValue datetimeRho = this.castToDateTimeValue(rho);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHms");
+			String strLho = datetimeLho.getDateTimeValue().format(formatter);
+			String strRho = datetimeRho.getDateTimeValue().format(formatter);
+
+			boolean result = this.operationOnString(strLho, strRho);
+
+			comparsionResult = this.createResult(datetimeLho, datetimeRho, result, env);
+		}
+	}
+
+	private void defaultDateComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		String strLho = "";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+		if (lho.getType() == DateTimeTypeValue.getInstance()) {
+			strLho = this.castToDateTimeValue(lho).getDateTimeValue().format(formatter);
+		} else if(lho.getType() == DateTypeValue.getInstance()) {
+			strLho = this.castToDateValue(lho).getDateValue().format(formatter);
+		} else {
+			return;
+		}
+
+		String strRho = "";
+		if (rho.getType() == DateTimeTypeValue.getInstance()) {
+			strRho = this.castToDateTimeValue(rho).getDateTimeValue().format(formatter);
+		} else if(rho.getType() == DateTypeValue.getInstance()) {
+			strRho = this.castToDateValue(rho).getDateValue().format(formatter);
+		} else {
+			return;
+		}
+		boolean result = this.operationOnString(strLho, strRho);
+
+		comparsionResult = this.createResult(lho, rho, result, env);
+	}
+
+	private void defaultNumericComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		double doubleLho = 0.0;
+		if (lho.getType() == IntTypeValue.getInstance()) {
+			doubleLho = this.castToIntegerValue(lho).getIntValue();
+		} else if(lho.getType() == DoubleTypeValue.getInstance()) {
+			doubleLho = this.castToDoubleValue(lho).getDoubleValue();
+		} else {
+			return;
+		}
+
+		double doubleRho = 0.0;
+		if (rho.getType() == IntTypeValue.getInstance()) {
+			doubleRho = this.castToIntegerValue(rho).getIntValue();
+		} else if(rho.getType() == DoubleTypeValue.getInstance()) {
+			doubleRho = this.castToDoubleValue(rho).getDoubleValue();
+		} else {
+			return;
+		}
+		boolean result = this.operationOnDouble(doubleLho, doubleRho);
+
+		comparsionResult = this.createResult(lho, rho, result, env);
+	}
+
+	private void differentClassComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		if (!lho.getClass().equals(rho.getClass())) {
+			boolean result = this.operationOnDifferentClasses(lho, rho);
+			comparsionResult = this.createResult(lho, rho, result, env);
+		}
+	}
+
+	private void stringComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		if (lho.getType() == rho.getType() && lho.getType() == StringTypeValue.getInstance()) {
+			StringValue strLho = this.castToStringValue(lho);
+			StringValue strRho = this.castToStringValue(rho);
+			boolean result = this.operationOnString(strLho.getStrValue(), strRho.getStrValue());
+
+			comparsionResult = this.createResult(strLho, strRho, result, env);
+		}
+	}
+
+	private void intComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		if (lho.getType() == rho.getType() && lho.getType() == IntTypeValue.getInstance()) {
+			IntegerValue intLho = this.castToIntegerValue(lho);
+			IntegerValue intRho = this.castToIntegerValue(rho);
+			boolean result = this.operationOnInt(intLho.getIntValue(), intRho.getIntValue());
+
+			comparsionResult = this.createResult(intLho, intRho, result, env);
+		}
+	}
+
+	private void nullComparsion(ValueInterface lho, ValueInterface rho, Environment env) {
+		if (lho.getType() == VoidTypeValue.getInstance()) {
+			boolean result = this.operationOnNull(lho, rho);
+			comparsionResult = this.createResult(lho, rho, result, env);
+		}
+	}
+
+	abstract protected boolean operationOnInt(int lho, int rho);
+	abstract protected boolean operationOnDouble(double lho, double rho);
+	abstract protected boolean operationOnString(String lho, String rho);
+	abstract protected boolean operationOnBoolean(boolean lho, boolean rho);
+	abstract protected boolean operationOnNull(ValueInterface lho, ValueInterface rho);
+	abstract protected boolean operationOnDifferentClasses(ValueInterface lho, ValueInterface rho);
+
+	private IntegerValue castToIntegerValue(ValueInterface value) {
+		return (IntegerValue) value;
+	}
+
+	private DoubleValue castToDoubleValue(ValueInterface value) {
+		return (DoubleValue) value;
+	}
+
+	private StringValue castToStringValue(ValueInterface value) {
+		return (StringValue) value;
+	}
+
+	private DateValue castToDateValue(ValueInterface value) {
+		return (DateValue) value;
+	}
+
+	private DateTimeValue castToDateTimeValue(ValueInterface value) {
+		return (DateTimeValue) value;
+	}
+
+	private BooleanValue castToBooleanValue(ValueInterface value) {
+		return (BooleanValue) value;
+	}
+
+	private BooleanValue createResult(ValueInterface lho, ValueInterface rho, boolean result, Environment env) {
+		return new BooleanValue(lho.evaluate(env) + " " + rho.evaluate(env), result);
+	}
+
+}
